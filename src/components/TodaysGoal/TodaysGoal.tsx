b5c6abc0223243c2ad119baf2f6aaf9a1d458980
Samuel Raducan <samuel.thembp@gmail.com>
2024-06-06 09:28:45 +0100

    Added todays goal heading and quote

--- src/components/TodaysGoal/TodaysGoal.tsx
+++ src/components/TodaysGoal/TodaysGoal.tsx
@@ -6,6 +6,10 @@ const TodaysGoal = () => {
             <h2 className="todays-goal__heading">
                 What is your main goal for today?
             </h2>
+            <p className="todays-goal__quote">
+                When you wake up in the morning you have two choices: go back to
+                sleep, or wake up and chase those dreams.
+            </p>
         </div>
     );
 };
