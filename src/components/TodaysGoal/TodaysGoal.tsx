8a4c7baf5a1adebebf58a813ef09ba8b7ce8ecfa
Samuel Raducan <samuel.thembp@gmail.com>
2024-06-06 15:22:30 +0100

    Added random quote generator

--- src/components/TodaysGoal/TodaysGoal.tsx
+++ src/components/TodaysGoal/TodaysGoal.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { Todo } from "../../types/todo";
+import { quotes } from "../../data/quotes";
 import "./TodaysGoal.scss";
 
 interface TodaysGoalProps {
@@ -6,15 +8,24 @@ interface TodaysGoalProps {
 }
 
 const TodaysGoal = ({ todos }: TodaysGoalProps) => {
+    const [quote, setQuote] = useState<string>("");
+
+    useEffect(() => {
+        const getRandomQuote = (): string => {
+            const randomIndex = Math.floor(Math.random() * quotes.length);
+            return quotes[randomIndex];
+        };
+        console.log(quotes.length);
+
+        setQuote(getRandomQuote());
+    }, []);
+
     return (
         <div className="todays-goal">
             <h2 className="todays-goal__heading">
                 What is your main goal for today?
             </h2>
-            <p className="todays-goal__quote">
-                When you wake up in the morning you have two choices: go back to
-                sleep, or wake up and chase those dreams.
-            </p>
+            <p className="todays-goal__quote">{quote}</p>
             {todos.length > 0 ? (
                 <h2 className="todays-goal__todo-heading">
                     Todos{" "}
